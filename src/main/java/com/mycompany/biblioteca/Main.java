a8f65461a8e5f527b0830f901f96f17475faa82d
luismcorzoc-droid <luismcorzoc@gmail.com>
2026-08-21 10:46:07 -0500

    feat: implementar CREATE de Cliente

--- src/main/java/com/mycompany/biblioteca/Main.java
+++ src/main/java/com/mycompany/biblioteca/Main.java
@@ -4,13 +4,43 @@
 
 package com.mycompany.biblioteca;
 
-/**
- *
- * @author ASUS
- */
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 
+    static ArrayList<Cliente> clientes = new ArrayList<>();
+    static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // Aquí irá el menú (Fase 8)
     }
+}
+
+public static void crearCliente() {
+
+    System.out.println("=== REGISTRAR CLIENTE ===");
+
+    System.out.print("Nombre: ");
+    String nombre = sc.nextLine();
+
+    System.out.print("Documento: ");
+    String documento = sc.nextLine();
+
+    System.out.print("Teléfono: ");
+    String telefono = sc.nextLine();
+
+    System.out.print("Código del cliente: ");
+    String codigoCliente = sc.nextLine();
+
+    Cliente cliente = new Cliente(
+            nombre,
+            documento,
+            telefono,
+            codigoCliente
+    );
+
+    Clientes.add(cliente);
+
+    System.out.println("Cliente creado correctamente.");
 }
